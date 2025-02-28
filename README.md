cbe39d78eb5818c70d4ce09065470352eae0a20a
harshaneo17 <harsha.god86@gmail.com>
2025-02-28 09:49:48 +0000

    added command line parser and readme file

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # smd-plc
 RTI connext based PLC module 
+
+The RTI DDS module is used to interface the PLC tags and control them using a publisher subscriber architecture
+
+To run the module start the docker container
+
+```bash
+docker build -t connext:smd-plc -f docker/Dockerfile --build-arg RTI_LICENSE_AGREEMENT_ACCEPTED=accepted --build-arg CONNEXT_VERSION=7.3.0 .
+```
+
+
+```bash
+docker run -it --rm --net=host -v "C:\Users\sriharsha.a\Downloads\rti_license.dat":/root/rti_license.dat connext:smd-plc
+```
+
+In the container run these commands
+
+```bash
+cd app
+```
+Run the tmux terminal which launches all the publishers and subscribers with the OPCUA to DDS gateway
+
+```bash
+./run_tmux.sh
+```
+
+
--- publisher_control/plc_publisher.py
+++ publisher_control/plc_publisher.py
@@ -1,14 +1,3 @@
-
-# (c) Copyright, Real-Time Innovations, 2022.  All rights reserved.
-# RTI grants Licensee a license to use, modify, compile, and create derivative
-# works of the software solely for use with RTI Connext DDS. Licensee may
-# redistribute copies of the software provided that all such copies are subject
-# to this license. The software is provided "as is", with no warranty of any
-# type, including any warranty for fitness for any purpose. RTI is under no
-# obligation to maintain or support the software. RTI shall not be liable for
-# any incidental or consequential damages arising out of the use or inability
-# to use the software.
-
 import time
 import argparse
 import sys
@@ -19,29 +8,19 @@ from plc import CameraControlCustom, PanAndTiltControlCustom, LampControlCustom
 class ControlPublisher:
 
     @staticmethod
-    def camera_publisher(domain_id: int, sample_count: int):
-
-        # A DomainParticipant allows an application to begin communicating in
-        # a DDS domain. Typically there is one DomainParticipant per application.
-        # DomainParticipant QoS is configured in USER_QOS_PROFILES.xml
-        participant = dds.DomainParticipant(domain_id)
-
-        # A Topic has a name and a datatype.
+    def camera_publisher(domain_id: float, sample_count: int, camera_id: float, power: bool, light: bool, focus: float, zoom: float):
+        participant = dds.DomainParticipant(int(domain_id))
         topic = dds.Topic(participant, "CameraSetterTopic", CameraControlCustom)
-
-        # DataWriter QoS is configured in USER_QOS_PROFILES.xml
         writer = dds.DataWriter(participant.implicit_publisher, topic)
         data = CameraControlCustom()        
 
         for count in range(sample_count):
-            # Catch control-C interrupt
             try:
-                # Modify the data to be sent here
-                data.cameraID = 9
-                data.power = True
-                data.light = True
-                data.focus = 22
-                data.zoom = 25               
+                data.cameraID = int(camera_id)
+                data.power = power
+                data.light = light
+                data.focus = focus
+                data.zoom = zoom               
                 
                 writer.write(data)
                 time.sleep(1)
@@ -51,28 +30,17 @@ class ControlPublisher:
         print("preparing to shut down CameraSetterTopic publisher...")
     
     @staticmethod
-    def lamp_publisher(domain_id: int, sample_count: int):
-
-        # A DomainParticipant allows an application to begin communicating in
-        # a DDS domain. Typically there is one DomainParticipant per application.
-        # DomainParticipant QoS is configured in USER_QOS_PROFILES.xml
-        participant = dds.DomainParticipant(domain_id)
-
-        # A Topic has a name and a datatype.
+    def lamp_publisher(domain_id: float, sample_count: int, lamp_id: float, intensity: float, power: bool):
+        participant = dds.DomainParticipant(int(domain_id))
         topic = dds.Topic(participant, "LampSetterTopic", LampControlCustom)
-
-        
-        # DataWriter QoS is configured in USER_QOS_PROFILES.xml
         writer = dds.DataWriter(participant.implicit_publisher, topic)
         data = LampControlCustom()        
 
         for count in range(sample_count):
-            # Catch control-C interrupt
             try:
-                # Modify the data to be sent here
-                data.lampID = 9
-                data.intensity = 80
-                data.power = True
+                data.lampID = int(lamp_id)
+                data.intensity = intensity
+                data.power = power
 
                 writer.write(data)
                 time.sleep(1)
@@ -82,29 +50,18 @@ class ControlPublisher:
         print("preparing to shut down LampControlCustom publisher...")
 
     @staticmethod
-    def panandtilt_publisher(domain_id: int, sample_count: int):
-
-        # A DomainParticipant allows an application to begin communicating in
-        # a DDS domain. Typically there is one DomainParticipant per application.
-        # DomainParticipant QoS is configured in USER_QOS_PROFILES.xml
-        participant = dds.DomainParticipant(domain_id)
-
-        # A Topic has a name and a datatype.
+    def panandtilt_publisher(domain_id: float, sample_count: int, panandtilt_id: float, x: float, y: float, z: float):
+        participant = dds.DomainParticipant(int(domain_id))
         topic = dds.Topic(participant, "PanAndTiltSetterTopic", PanAndTiltControlCustom)
-
-        
-        # DataWriter QoS is configured in USER_QOS_PROFILES.xml
         writer = dds.DataWriter(participant.implicit_publisher, topic)
         data = PanAndTiltControlCustom()        
 
         for count in range(sample_count):
-            # Catch control-C interrupt
             try:
-                # Modify the data to be sent here
-                data.panandtiltID = 9
-                data.x = 80
-                data.y = 20
-                data.z = 23
+                data.panandtiltID = int(panandtilt_id)
+                data.x = x
+                data.y = y
+                data.z = z
 
                 writer.write(data)
                 time.sleep(1)
@@ -113,11 +70,36 @@ class ControlPublisher:
 
         print("preparing to shut down...")
 
-
 if __name__ == "__main__":
-    func1 = threading.Thread(target=ControlPublisher.camera_publisher(domain_id=6,sample_count=1))
+    parser = argparse.ArgumentParser(description="Control Publisher")
+    parser.add_argument("--domain_id", type=float, default=6.0, help="Domain ID")
+    parser.add_argument("--sample_count", type=int, default=1, help="Sample count")
+    
+    # Camera arguments
+    parser.add_argument("--camera_id", type=float, default=9.0, help="Camera ID")
+    parser.add_argument("--camera_power", type=bool, default=True, help="Camera power")
+    parser.add_argument("--camera_light", type=bool, default=True, help="Camera light")
+    parser.add_argument("--camera_focus", type=float, default=22.0, help="Camera focus")
+    parser.add_argument("--camera_zoom", type=float, default=25.0, help="Camera zoom")
+    
+    # Lamp arguments
+    parser.add_argument("--lamp_id", type=float, default=9.0, help="Lamp ID")
+    parser.add_argument("--lamp_intensity", type=float, default=80.0, help="Lamp intensity")
+    parser.add_argument("--lamp_power", type=bool, default=True, help="Lamp power")
+    
+    # Pan and Tilt arguments
+    parser.add_argument("--panandtilt_id", type=float, default=9.0, help="Pan and Tilt ID")
+    parser.add_argument("--panandtilt_x", type=float, default=80.0, help="Pan and Tilt X")
+    parser.add_argument("--panandtilt_y", type=float, default=20.0, help="Pan and Tilt Y")
+    parser.add_argument("--panandtilt_z", type=float, default=23.0, help="Pan and Tilt Z")
+    
+    args = parser.parse_args()
+
+    func1 = threading.Thread(target=ControlPublisher.camera_publisher, args=(args.domain_id, args.sample_count, args.camera_id, args.camera_power, args.camera_light, args.camera_focus, args.camera_zoom))
     func1.start()
-    func2 = threading.Thread(target=ControlPublisher.lamp_publisher(domain_id=6,sample_count=1))
+    
+    func2 = threading.Thread(target=ControlPublisher.lamp_publisher, args=(args.domain_id, args.sample_count, args.lamp_id, args.lamp_intensity, args.lamp_power))
     func2.start()
-    func3 = threading.Thread(target=ControlPublisher.panandtilt_publisher(domain_id=6,sample_count=1))
+    
+    func3 = threading.Thread(target=ControlPublisher.panandtilt_publisher, args=(args.domain_id, args.sample_count, args.panandtilt_id, args.panandtilt_x, args.panandtilt_y, args.panandtilt_z))
     func3.start()
