b09b88d9087fc8893905a130dc88102a8b142d40
harshaneo17 <harsha.god86@gmail.com>
2025-03-26 15:55:05 +0000

    changes to subscriber

--- app/plc_subscriber.cxx
+++ app/plc_subscriber.cxx
@@ -32,15 +32,13 @@
 #include "application.hpp"
 #include "plc_publisher.hpp"  // for command line parsing and ctrl-c
 
-static std::mutex work_mutex;
 
-int process_camera_data(dds::sub::DataReader< ::CameraControlCustom> reader,unsigned int domain_id, unsigned int sample_count)
+int process_camera_data(dds::sub::DataReader< ::CameraControlCustom> reader,dds::pub::DataWriter< ::CameraControl> writer)
 {
     int count = 0;
     dds::sub::LoanedSamples< ::CameraControlCustom> samples = reader.take();
     for (auto sample : samples) {
         if (sample.info().valid()) {
-            //std::lock_guard<std::mutex> lock(work_mutex);
             count++;
             std::cout << sample.data() << std::endl;
             
@@ -52,7 +50,7 @@ int process_camera_data(dds::sub::DataReader< ::CameraControlCustom> reader,unsi
             camera_data.focus = sample.data().focus();
             camera_data.zoom = sample.data().zoom();
             
-            camera_publisher(domain_id, sample_count, camera_data);
+            camera_publisher(camera_data,writer);
 
             
         } else {
@@ -63,13 +61,12 @@ int process_camera_data(dds::sub::DataReader< ::CameraControlCustom> reader,unsi
     return count;
 }
 
-int process_lamp_data(dds::sub::DataReader< ::LampControlCustom> reader, unsigned int domain_id, unsigned int sample_count)
+int process_lamp_data(dds::sub::DataReader< ::LampControlCustom> reader,dds::pub::DataWriter< ::LampControl> writer)
 {
     int count = 0;
     dds::sub::LoanedSamples< ::LampControlCustom> samples = reader.take();
     for (auto sample : samples) {
         if (sample.info().valid()) {
-            //std::lock_guard<std::mutex> lock(work_mutex);
             count++;
             std::cout << sample.data() << std::endl;
             
@@ -78,7 +75,7 @@ int process_lamp_data(dds::sub::DataReader< ::LampControlCustom> reader, unsigne
             lamp_data.intensity = sample.data().intensity();
             lamp_data.power = sample.data().power();
 
-            lamp_publisher(domain_id, sample_count, lamp_data);
+            lamp_publisher(lamp_data,writer);
             
         } else {
             std::cout << "Instance state changed to "
@@ -88,7 +85,7 @@ int process_lamp_data(dds::sub::DataReader< ::LampControlCustom> reader, unsigne
     return count;
 }
 
-int process_panandtilt_data(dds::sub::DataReader< ::PanAndTiltControlCustom> reader,unsigned int domain_id, unsigned int sample_count)
+int process_panandtilt_data(dds::sub::DataReader< ::PanAndTiltControlCustom> reader,dds::pub::DataWriter< ::PanAndTiltControl> writer)
 {
     int count = 0;
     dds::sub::LoanedSamples< ::PanAndTiltControlCustom> samples = reader.take();
@@ -103,7 +100,7 @@ int process_panandtilt_data(dds::sub::DataReader< ::PanAndTiltControlCustom> rea
             pan_and_tilt_data.x = sample.data().x();
             pan_and_tilt_data.y = sample.data().y();
             pan_and_tilt_data.z = sample.data().z();
-            panandtilt_publisher(domain_id, sample_count, pan_and_tilt_data);
+            panandtilt_publisher(pan_and_tilt_data,writer);
             
         } else {
             std::cout << "Instance state changed to "
@@ -113,102 +110,105 @@ int process_panandtilt_data(dds::sub::DataReader< ::PanAndTiltControlCustom> rea
     return count;
 }
 
-// int process_ptposition_data(dds::sub::DataReader< ::PanAndTiltPositionSubscriber> reader,unsigned int domain_id, unsigned int sample_count)
+int process_ptposition_data(dds::sub::DataReader< ::PanAndTiltPositionSubscriber> reader,dds::pub::DataWriter< ::PanAndTiltPositionPublisher> writer)
 
-// {
+{
 
-//     int count = 0;
-//     dds::sub::LoanedSamples< ::PanAndTiltPositionSubscriber> samples = reader.take();
+    int count = 0;
+    dds::sub::LoanedSamples< ::PanAndTiltPositionSubscriber> samples = reader.take();
 
-//     for (auto sample : samples) {
-//         if (sample.info().valid()) {
-//             count++;
-//             std::cout << sample.data() << std::endl;
+    for (auto sample : samples) {
+        if (sample.info().valid()) {
+            count++;
+            std::cout << sample.data() << std::endl;
 
-//             HelperMethods::PanAndTiltPositionStruct pt_position_data;
-//             pt_position_data.pan_one = sample.data().pan_one();
-//             pt_position_data.tilt_one = sample.data().tilt_one();
-//             pt_position_data.pan_two = sample.data().pan_two();
-//             pt_position_data.tilt_two = sample.data().tilt_two();
-//             pt_position_data.pan_three = sample.data().pan_three();
-//             pt_position_data.tilt_three = sample.data().tilt_three();
-//             pt_position_data.pan_four = sample.data().pan_four();
-//             pt_position_data.tilt_four = sample.data().tilt_four();
-//             pt_position_data.pan_five = sample.data().pan_five();
-//             pt_position_data.tilt_five = sample.data().tilt_five();
-//             pt_position_data.pan_six = sample.data().pan_six();
-//             pt_position_data.tilt_six = sample.data().tilt_six();
-//             pt_position_data.pan_seven = sample.data().pan_seven();
-//             pt_position_data.tilt_seven = sample.data().tilt_seven();
-//             pt_position_data.pan_eight = sample.data().pan_eight();
-//             pt_position_data.tilt_eight = sample.data().tilt_eight();
+            HelperMethods::PanAndTiltPositionStruct pt_position_data;
+            pt_position_data.pan_one = sample.data().pan_one();
+            pt_position_data.tilt_one = sample.data().tilt_one();
+            pt_position_data.pan_two = sample.data().pan_two();
+            pt_position_data.tilt_two = sample.data().tilt_two();
+            pt_position_data.pan_three = sample.data().pan_three();
+            pt_position_data.tilt_three = sample.data().tilt_three();
+            pt_position_data.pan_four = sample.data().pan_four();
+            pt_position_data.tilt_four = sample.data().tilt_four();
+            pt_position_data.pan_five = sample.data().pan_five();
+            pt_position_data.tilt_five = sample.data().tilt_five();
+            pt_position_data.pan_six = sample.data().pan_six();
+            pt_position_data.tilt_six = sample.data().tilt_six();
+            pt_position_data.pan_seven = sample.data().pan_seven();
+            pt_position_data.tilt_seven = sample.data().tilt_seven();
+            pt_position_data.pan_eight = sample.data().pan_eight();
+            pt_position_data.tilt_eight = sample.data().tilt_eight();
 
-//             for (int i = 0; i <= 7; i++) {
-//                 HelperMethods::PanAndTiltPositionPublisherStruct pub_data;
-//                 pub_data.panandtiltID = i;
-//                 std::cout << "Executed case: " << i << std::endl;
-//                 switch(i) {
-//                     case 0:
-//                         pub_data.pan = pt_position_data.pan_one;
-//                         pub_data.tilt = pt_position_data.tilt_one;
-//                         //pt_position_publisher(domain_id, sample_count, pub_data);
-//                         break;
-//                     case 1:
-//                         pub_data.pan = pt_position_data.pan_two;
-//                         pub_data.tilt = pt_position_data.tilt_two;
-//                         //pt_position_publisher(domain_id, sample_count, pub_data);
-//                         break;
-//                     case 2:
-//                         pub_data.pan = pt_position_data.pan_three;
-//                         pub_data.tilt = pt_position_data.tilt_three;
-//                         //pt_position_publisher(domain_id, sample_count, pub_data);
-//                         break;
-//                     case 3:
-//                         pub_data.pan = pt_position_data.pan_four;
-//                         pub_data.tilt = pt_position_data.tilt_four;
-//                         //pt_position_publisher(domain_id, sample_count, pub_data);
-//                         break;
-//                     case 4:
-//                         pub_data.pan = pt_position_data.pan_five;
-//                         pub_data.tilt = pt_position_data.tilt_five;
-//                         //pt_position_publisher(domain_id, sample_count, pub_data);
-//                         break;
-//                     case 5:
-//                         pub_data.pan = pt_position_data.pan_six;
-//                         pub_data.tilt = pt_position_data.tilt_six;
-//                         //pt_position_publisher(domain_id, sample_count, pub_data);
-//                         break;
-//                     case 6:
-//                         pub_data.pan = pt_position_data.pan_seven;
-//                         pub_data.tilt = pt_position_data.tilt_seven;
-//                         //pt_position_publisher(domain_id, sample_count, pub_data);
-//                         break;
-//                     case 7: 
-//                         pub_data.pan = pt_position_data.pan_eight;
-//                         pub_data.tilt = pt_position_data.tilt_eight;
-//                         //pt_position_publisher(domain_id, sample_count, pub_data);
-//                         break;
-//                 }
-//             }
-//         } else {
-//             std::cout << "Instance state changed to "
-//             << sample.info().state().instance_state() << std::endl;
-//         }
-//     }
-//     return count;
-// }
+            for (int i = 0; i <= 7; i++) {
+                HelperMethods::PanAndTiltPositionPublisherStruct pub_data;
+                pub_data.panandtiltID = i;
+                std::cout << "Executed case: " << i << std::endl;
+                switch(i) {
+                    case 0:
+                        pub_data.pan = pt_position_data.pan_one;
+                        pub_data.tilt = pt_position_data.tilt_one;
+                        pt_position_publisher(pub_data, writer);
+                        break;
+                    case 1:
+                        pub_data.pan = pt_position_data.pan_two;
+                        pub_data.tilt = pt_position_data.tilt_two;
+                        pt_position_publisher(pub_data, writer);
+                        break;
+                    case 2:
+                        pub_data.pan = pt_position_data.pan_three;
+                        pub_data.tilt = pt_position_data.tilt_three;
+                        pt_position_publisher(pub_data, writer);
+                        break;
+                    case 3:
+                        pub_data.pan = pt_position_data.pan_four;
+                        pub_data.tilt = pt_position_data.tilt_four;
+                        pt_position_publisher(pub_data, writer);
+                        break;
+                    case 4:
+                        pub_data.pan = pt_position_data.pan_five;
+                        pub_data.tilt = pt_position_data.tilt_five;
+                        pt_position_publisher(pub_data, writer);
+                        break;
+                    case 5:
+                        pub_data.pan = pt_position_data.pan_six;
+                        pub_data.tilt = pt_position_data.tilt_six;
+                        pt_position_publisher(pub_data, writer);
+                        break;
+                    case 6:
+                        pub_data.pan = pt_position_data.pan_seven;
+                        pub_data.tilt = pt_position_data.tilt_seven;
+                        pt_position_publisher(pub_data, writer);
+                        break;
+                    case 7: 
+                        pub_data.pan = pt_position_data.pan_eight;
+                        pub_data.tilt = pt_position_data.tilt_eight;
+                        pt_position_publisher(pub_data, writer);
+                        break;
+                }
+            }
+        } else {
+            std::cout << "Instance state changed to "
+            << sample.info().state().instance_state() << std::endl;
+        }
+    }
+    return count;
+}
 
 void camera_subscriber(unsigned int domain_id, unsigned int sample_count)
 {
 
     dds::domain::DomainParticipant participant(domain_id);
+    dds::sub::Subscriber subscriber(participant);
+    dds::pub::Publisher publisher(participant);
 
-    // Create a Topic with a name and a datatype
     dds::topic::Topic< ::CameraControlCustom> topic(participant, "CameraSetterTopic");
+    dds::topic::Topic< ::CameraControl> topic_publisher(participant, "CameraControlTopic");
 
     // Create a Subscriber and DataReader with default Qos
-    dds::sub::Subscriber subscriber(participant);
+
     dds::sub::DataReader< ::CameraControlCustom> reader(subscriber, topic); 
+    dds::pub::DataWriter< ::CameraControl> writer(publisher, topic_publisher);
 
     // Create a ReadCondition for any data received on this reader and set a
     // handler to process the data
@@ -216,7 +216,7 @@ void camera_subscriber(unsigned int domain_id, unsigned int sample_count)
     dds::sub::cond::ReadCondition read_condition(
         reader,
         dds::sub::status::DataState::any(),
-        [&, reader]() { samples_read += process_camera_data(reader,domain_id,sample_count); });
+        [&, reader]() { samples_read += process_camera_data(reader,writer); });
 
     // WaitSet will be woken when the attached condition is triggered
     dds::core::cond::WaitSet waitset;
@@ -237,10 +237,15 @@ void lamp_subscriber(unsigned int domain_id, unsigned int sample_count)
 
     // Create a Topic with a name and a datatype
     dds::topic::Topic< ::LampControlCustom> topic(participant, "LampSetterTopic");
+    dds::topic::Topic< ::LampControl> topic_publisher(participant, "LampControlTopic");
 
     // Create a Subscriber and DataReader with default Qos
     dds::sub::Subscriber subscriber(participant);
+    dds::pub::Publisher publisher(participant);
+
+
     dds::sub::DataReader< ::LampControlCustom> reader(subscriber, topic); 
+    dds::pub::DataWriter< ::LampControl> writer(publisher, topic_publisher);
 
     // Create a ReadCondition for any data received on this reader and set a
     // handler to process the data
@@ -248,7 +253,7 @@ void lamp_subscriber(unsigned int domain_id, unsigned int sample_count)
     dds::sub::cond::ReadCondition read_condition(
         reader,
         dds::sub::status::DataState::any(),
-        [&, reader]() { samples_read += process_lamp_data(reader,domain_id,sample_count); });
+        [&, reader]() { samples_read += process_lamp_data(reader,writer); });
 
     // WaitSet will be woken when the attached condition is triggered
     dds::core::cond::WaitSet waitset;
@@ -267,20 +272,19 @@ void panandtilt_subscriber(unsigned int domain_id, unsigned int sample_count)
 
     dds::domain::DomainParticipant participant(domain_id);
 
-    // Create a Topic with a name and a datatype
-    dds::topic::Topic< ::PanAndTiltControlCustom> topic(participant, "PanAndTiltSetterTopic");
-
-    // Create a Subscriber and DataReader with default Qos
     dds::sub::Subscriber subscriber(participant);
-    dds::sub::DataReader< ::PanAndTiltControlCustom> reader(subscriber, topic); 
+    dds::pub::Publisher publisher(participant);
+    dds::topic::Topic< ::PanAndTiltControlCustom> topic(participant, "PanAndTiltSetterTopic");
+    dds::topic::Topic< ::PanAndTiltControl> topic_publisher(participant, "PanAndTiltControlTopic");
 
-    // Create a ReadCondition for any data received on this reader and set a
-    // handler to process the data
+    dds::sub::DataReader< ::PanAndTiltControlCustom> reader(subscriber, topic);
+    dds::pub::DataWriter< ::PanAndTiltControl> writer(publisher, topic_publisher); 
+
     unsigned int samples_read = 0;
     dds::sub::cond::ReadCondition read_condition(
         reader,
         dds::sub::status::DataState::any(),
-        [&, reader]() { samples_read += process_panandtilt_data(reader,domain_id,sample_count); });
+        [&, reader]() { samples_read += process_panandtilt_data(reader,writer); });
 
     // WaitSet will be woken when the attached condition is triggered
     dds::core::cond::WaitSet waitset;
@@ -294,36 +298,39 @@ void panandtilt_subscriber(unsigned int domain_id, unsigned int sample_count)
     }
 }
 
-// void panandtilt_position_subscriber(unsigned int domain_id, unsigned int sample_count)
-// {
-//     dds::domain::DomainParticipant participant(domain_id);
+void panandtilt_position_subscriber(unsigned int domain_id, unsigned int sample_count)
+{
+    dds::domain::DomainParticipant participant(domain_id);
 
-//     // Create a Topic with a name and a datatype
-//     dds::topic::Topic< ::PanAndTiltPositionSubscriber> panandtilt_position_topic(participant, "PanAndTiltPositionSubscriberTopic");
+    dds::sub::Subscriber subscriber(participant);
+    dds::pub::Publisher publisher(participant);
 
-//     // Create a Subscriber and DataReader with default Qos
-//     dds::sub::Subscriber subscriber(participant);
-//     dds::sub::DataReader< ::PanAndTiltPositionSubscriber> reader(subscriber, panandtilt_position_topic); 
+    dds::topic::Topic< ::PanAndTiltPositionPublisher> topic(participant, "PanAndTiltPositionTopic");
+    dds::topic::Topic< ::PanAndTiltPositionSubscriber> panandtilt_position_topic(participant, "PanAndTiltPositionSubscriberTopic");
 
-//     // Create a ReadCondition for any data received on this reader and set a
-//     // handler to process the data
-//     unsigned int samples_read = 0;
-//     dds::sub::cond::ReadCondition read_condition(
-//         reader,
-//         dds::sub::status::DataState::any(),
-//         [&, reader]() { samples_read += process_ptposition_data(reader,domain_id,sample_count); });
+    // Create a DataWriter with default QoS
+    dds::sub::DataReader< ::PanAndTiltPositionSubscriber> reader(subscriber, panandtilt_position_topic); 
+    dds::pub::DataWriter< ::PanAndTiltPositionPublisher> writer(publisher, topic);
 
-//     // WaitSet will be woken when the attached condition is triggered
-//     dds::core::cond::WaitSet waitset;
-//     waitset += read_condition;
+    // Create a ReadCondition for any data received on this reader and set a
+    // handler to process the data
+    unsigned int samples_read = 0;
+    dds::sub::cond::ReadCondition read_condition(
+        reader,
+        dds::sub::status::DataState::any(),
+        [&, reader]() { samples_read += process_ptposition_data(reader,writer); });
 
-//     while (!application::shutdown_requested && samples_read < sample_count) {
-//         std::cout << "::PanAndTiltPositionSubscriber subscriber sleeping up to 1 sec..." << std::endl;
+    // WaitSet will be woken when the attached condition is triggered
+    dds::core::cond::WaitSet waitset;
+    waitset += read_condition;
 
-//         // Run the handlers of the active conditions. Wait for up to 1 second.
-//         waitset.dispatch(dds::core::Duration(1));
-//     }
-// }
+    //while (!application::shutdown_requested && samples_read < sample_count) {
+        std::cout << "::PanAndTiltPositionSubscriber subscriber sleeping up to 1 sec..." << std::endl;
+
+        // Run the handlers of the active conditions. Wait for up to 1 second.
+        waitset.dispatch(dds::core::Duration(1));
+    //}
+}
 
 int main(int argc, char *argv[])
 {
@@ -354,7 +361,7 @@ int main(int argc, char *argv[])
     threads.emplace_back(run_subscriber, camera_subscriber, 0);
     threads.emplace_back(run_subscriber, lamp_subscriber, 1);
     threads.emplace_back(run_subscriber, panandtilt_subscriber, 2);
-    // threads.emplace_back(run_subscriber, panandtilt_position_subscriber, 3);
+    threads.emplace_back(run_subscriber, panandtilt_position_subscriber, 3);
 
     for (auto& thread : threads) {
         thread.join();
